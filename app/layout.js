ced8d6a8ac18e532d24e490715256dfc2e7cb438
Kai Kuller <kavakai@gmail.com>
2023-05-10 12:09:53 -1000

    Create initial components

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import React from "react"
 import './styles/globals.css'
 import { Inter } from 'next/font/google'
 
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Product, HeroBanner, FooterBanner } from '@/components'
 
 const Home = () => {
   return (
