e404da416f2420889fe1c91e0aa869921e9af12e
Kai Kuller <kavakai@gmail.com>
2023-05-14 08:09:59 -1000

    Connect sanity to app

--- app/index.js
+++ app/index.js
@@ -3,10 +3,12 @@ import React from 'react';
 import { client } from '@/lib/client';
 import { Product, HeroBanner, FooterBanner } from '@/components';
 
-const Home = ({ products, bannerData }) => {
+const Home = async ({ products, bannerData }) => {
+  const data = await (getData());
   return (
     <>
-      <HeroBanner bannerData={bannerData} />
+    {console.log(data, "in component")}
+      <HeroBanner />
 
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
@@ -14,7 +16,7 @@ const Home = ({ products, bannerData }) => {
       </div>
 
       <div className='products-container'>
-        {products?.map((product) => product.name)}
+        {data?.map((product) => product.name)}
       </div>
 
       <FooterBanner />
@@ -23,15 +25,18 @@ const Home = ({ products, bannerData }) => {
 };
 
 
-export const getServerSideProps = async () => {
+export const getData = async () => {
   const query = '*[_type == "product"]';
-  const products = await client.fetch(query);
+  const products = await client.fetch(query)
+  .then((data) => data);
   const bannerQuery = '*[_type == "banner"]';
-  const bannerData = await client.fetch(bannerQuery);
+  const bannerData = await client.fetch(bannerQuery)
+  .then((data) => data);
 
-  return {
-    props: { products, bannerData }
-  };
+  console.log(products, "prods")
+
+
+  return products;
 };
 
 export default Home
